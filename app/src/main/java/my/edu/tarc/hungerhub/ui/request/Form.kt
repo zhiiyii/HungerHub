ed6c089c86a0c26fe65c87bb212b63b91a94e4bb
zhiiyii <zylow1@gmail.com>
2022-12-27 13:25:50 +0800

    zy: request food done

---
+++ app/src/main/java/my/edu/tarc/hungerhub/ui/request/Form.kt
@@ -0,0 +1,8 @@
+package my.edu.tarc.hungerhub.ui.request
+
+data class Form (
+    val maritalStatus: String,
+    val jobStatus: String,
+    val income: Int,
+    val reason: String
+    )
--- app/src/main/java/my/edu/tarc/hungerhub/ui/request/RequestFragment.kt
+++ app/src/main/java/my/edu/tarc/hungerhub/ui/request/RequestFragment.kt
@@ -1,6 +1,8 @@
 package my.edu.tarc.hungerhub.ui.request
 
 import android.app.Activity
+import android.app.AlertDialog
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -13,6 +15,8 @@ import androidx.core.view.MenuHost
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Lifecycle
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.snackbar.Snackbar
 import my.edu.tarc.hungerhub.R
 import my.edu.tarc.hungerhub.databinding.FragmentRequestBinding
 
@@ -56,7 +60,7 @@ class RequestFragment : Fragment() {
         }
 
         binding.buttonSubmit.setOnClickListener {
-            // TODO: check empty fields
+            // check empty fields
             if (binding.radioGroupMarital.checkedRadioButtonId == -1) {
                 Toast.makeText(this.requireContext(), getString(R.string.marital_required), Toast.LENGTH_LONG).show()
                 return@setOnClickListener
@@ -74,22 +78,50 @@ class RequestFragment : Fragment() {
                 return@setOnClickListener
             }
 
-            // TODO: check format
-            if (binding.editTextIncome.text.toString().toInt() < 0) {
-                binding.editTextIncome.error = getString(R.string.invalid_input)
-                return@setOnClickListener
-            }
-
-            // TODO: check agreement of TNC
+            // check agreement of TNC
             if (!binding.checkBoxTnc.isChecked) {
                 Toast.makeText(this.requireContext(), getString(R.string.tnc_required), Toast.LENGTH_LONG).show()
                 return@setOnClickListener
             }
 
-            // TODO: prompt confirmation
+            // alert dialog to prompt confirmation
+            val builder = AlertDialog.Builder(this.requireContext())
+            builder.setTitle(R.string.dialog_title)
+            builder.setMessage(R.string.dialog_message)
+            builder.setIcon(R.drawable.alert)
 
-            // TODO: submit form & store data
+            // press submit in dialog
+            builder.setPositiveButton(R.string.submit) { dialogInterface, which ->
+                // create a reference to the Shared Preference
+                val sharedPref = activity?.getSharedPreferences(getString(R.string.form_pref),
+                    Context.MODE_PRIVATE)?: return@setPositiveButton
 
+                with (sharedPref.edit()) {
+                    val form = Form(
+                        binding.radioGroupMarital.toString(),
+                        binding.radioGroupJob.toString(),
+                        binding.editTextIncome.text.toString().toInt(),
+                        binding.editTextReason.text.toString()
+                    )
+                    putString(getString(R.string.marital_status), form.maritalStatus)
+                    putString(getString(R.string.job_status), form.jobStatus)
+                    putString(getString(R.string.income), form.income.toString())
+                    putString(getString(R.string.reason), form.reason)
+                    apply()
+                }
+
+                Snackbar.make(this.requireActivity().findViewById(R.id.constraintLayout_request),
+                    getString(R.string.form_submitted), Snackbar.LENGTH_SHORT).show()
+            }
+
+            // press cancel in dialog
+            builder.setNeutralButton(R.string.cancel) { dialogInterface, which ->
+                Toast.makeText(this.requireContext(), R.string.action_cancelled, Toast.LENGTH_SHORT).show()
+            }
+
+            val alertDialog: AlertDialog = builder.create()
+            alertDialog.setCancelable(false)
+            alertDialog.show()
         }
     }
 
@@ -97,6 +129,4 @@ class RequestFragment : Fragment() {
         super.onDestroyView()
         _binding = null
     }
-
-
 }
