38fa69c0fc2399947deeb00886d32f51c6e7079c
Shuyilow <lowshuyi2002@gmail.com>
2022-12-31 13:14:07 +0800

    Donation page.

--- app/src/main/java/my/edu/tarc/hungerhub/ui/donation/DonationFragment2.kt
+++ app/src/main/java/my/edu/tarc/hungerhub/ui/donation/DonationFragment2.kt
@@ -5,37 +5,29 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
-import my.edu.tarc.hungerhub.R
-import my.edu.tarc.hungerhub.databinding.FragmentDonationBinding
+import my.edu.tarc.hungerhub.databinding.FragmentDonation2Binding
 
-class DonationFragment : Fragment() {
-
-    private var _binding: FragmentDonationBinding? = null
+class DonationFragment2: Fragment() {
+    private var _binding: FragmentDonation2Binding? = null
     private val binding get() = _binding!!
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentDonationBinding.inflate(inflater, container, false)
+        _binding = FragmentDonation2Binding.inflate(inflater, container, false)
         return binding.root
     }
 
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.buttonSubmitDonation.setOnClickListener {
-            findNavController().navigate(R.id.action_nav_donation_to_nav_donation2)
-        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
-
-
-
-
 }
