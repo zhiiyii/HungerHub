397c3db80068cd486a5b573dd00ab31e0eb6af84
zhiiyii <zylow1@gmail.com>
2023-01-01 16:08:50 +0800

    zy: update request status

---
+++ app/src/main/java/my/edu/tarc/hungerhub/ui/request/RequestDatabase.kt
@@ -0,0 +1,37 @@
+package my.edu.tarc.hungerhub.ui.request
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = arrayOf(Request::class), version = 1, exportSchema = false)
+abstract class RequestDatabase: RoomDatabase() {
+    abstract fun requestDao(): RequestDao
+
+    companion object {
+        // Singleton prevents multiple instance of the database opening at the same time
+        @Volatile // can be destroyed, uninstall app, data destroyed
+        private var INSTANCE: RequestDatabase? = null
+
+        fun getDatabase(context: Context): RequestDatabase {
+            val tempInstance = INSTANCE
+
+            if (tempInstance != null) { // database exist
+                return tempInstance
+            }
+
+            // Create an instance of the database
+            synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    RequestDatabase::class.java,
+                    "request_db"
+                ).build()
+
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
+}
