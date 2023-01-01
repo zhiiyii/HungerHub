67a38b149567fce3cfd4234ffa6b606c984e5e33
Shuyilow <lowshuyi2002@gmail.com>
2023-01-01 20:19:17 +0800

    Donation page ver2

--- app/src/main/java/my/edu/tarc/hungerhub/ui/donation/DonationFragment.kt
+++ app/src/main/java/my/edu/tarc/hungerhub/ui/donation/DonationFragment.kt
@@ -25,10 +25,48 @@ class DonationFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        var donationAmt = 0
         binding.buttonSubmitDonation.setOnClickListener {
             findNavController().navigate(R.id.action_nav_donation_to_nav_donation2)
         }
+
+//        binding.buttonDonation50.setOnClickListener {
+//            donationAmt = 50
+//        }
+//
+//        binding.buttonDonation100.setOnClickListener {
+//            donationAmt = 100
+//        }
+//        binding.buttonDonation500.setOnClickListener {
+//            donationAmt = 500
+//        }
+//
+//        binding.buttonDonation1000.setOnClickListener {
+//            donationAmt = 1000
+//        }
+
+
+//        binding.buttonSubmitDonation.setOnClickListener {
+//            if(binding.editTextDonationPrice.text.isEmpty()){
+//                binding.editTextDonationPrice.setError(getString(R.string.value_required))
+//            }else{
+//                if(isNumericToX(binding.editTextDonationPrice.text.toString())){
+//                    donationAmt = binding.editTextDonationPrice.text.toString().toInt()
+//                }else{
+//                    binding.editTextDonationPrice.setError(getString(R.string.invalid_format))
+//                }
+//                //TODO:validation for double or non-numeric
+////                donationAmt = binding.editTextDonationPrice.text.toString().toInt()
+//            }
+//        }
     }
+
+
+
+    fun isNumericToX(toCheck: String): Boolean {
+        return toCheck.toDoubleOrNull() != null
+    }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
