6fd1df5fd4172be05200ca52fdaa51197f31e7a0
zhiiyii <zylow1@gmail.com>
2023-01-04 22:02:20 +0800

    navigation drawer

--- app/src/main/java/my/edu/tarc/hungerhub/MainActivity.kt
+++ app/src/main/java/my/edu/tarc/hungerhub/MainActivity.kt
@@ -1,6 +1,10 @@
 package my.edu.tarc.hungerhub
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.TextView
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -9,6 +13,10 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import my.edu.tarc.hungerhub.databinding.ActivityMainBinding
 import my.edu.tarc.hungerhub.ui.donation.DonationFragment
 
@@ -36,11 +44,54 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
 
+        readLoginData()
+
+        val sharedPref = this.getSharedPreferences("Login", Context.MODE_PRIVATE)
+        if (sharedPref != null) {
+            val name = sharedPref.getString("name", null)
+            val email = sharedPref.getString("email", null)
+            val phone = sharedPref.getString("phoneNo", null)
+
+            val headerView = navView.getHeaderView(0)
+            headerView.findViewById<TextView>(R.id.textViewName).text = name
+            headerView.findViewById<TextView>(R.id.textViewEmail).text = email
+            headerView.findViewById<TextView>(R.id.textViewPhone).text = phone
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    private fun readLoginData() {
+        val referenceUser = FirebaseDatabase.getInstance().getReference(getString(R.string.firebase_user))
+
+        val sharedPref = this.getSharedPreferences("Login", Context.MODE_PRIVATE)
+        val loginIc = sharedPref?.getString("ic", null)
+        val findUser = referenceUser.orderByChild("ic").equalTo(loginIc)
+
+        findUser.addListenerForSingleValueEvent(object: ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                if (dataSnapshot.exists() && loginIc != null) {
+                    val children = loginIc.let { it1 -> dataSnapshot.child(it1) }
+                    val name = children.child("name").value.toString()
+                    val email = children.child("email").value.toString()
+                    val phoneNo = children.child("phoneNo").value.toString()
+
+                    with(sharedPref.edit()) {
+                        this?.clear()
+                        this?.putString("name", name)
+                        this?.putString("email", email)
+                        this?.putString("phone", phoneNo)
+                        this?.apply()
+                    }
+                }
+            }
+            override fun onCancelled(databaseError: DatabaseError) {
+                Log.e("firebase", "firebase error")
+            }
+        })
     }
 
 }
