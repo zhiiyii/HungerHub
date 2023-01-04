c8f03661fff3bcbbe97639ad24bf59fd12131e5a
zhiiyii <zylow1@gmail.com>
2023-01-04 20:34:24 +0800

    improve main menu

--- app/src/main/java/my/edu/tarc/hungerhub/MainMenuFragment.kt
+++ app/src/main/java/my/edu/tarc/hungerhub/MainMenuFragment.kt
@@ -1,12 +1,13 @@
 package my.edu.tarc.hungerhub
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import my.edu.tarc.hungerhub.databinding.FragmentHomeBinding
 import my.edu.tarc.hungerhub.databinding.FragmentMainMenuBinding
 
 class MainMenuFragment: Fragment()  {
@@ -23,22 +24,40 @@ class MainMenuFragment: Fragment()  {
         return binding.root
     }
 
-
-
     override fun onStart() {
         super.onStart()
+
         binding.imageButtonDonate.setOnClickListener{
+            findNavController().navigate(R.id.action_mainMenuFragment_to_nav_donation)
+        }
+        binding.textViewDonateMainMenu.setOnClickListener {
             findNavController().navigate(R.id.action_mainMenuFragment_to_nav_donation)
         }
 
         binding.imageButtonRequest.setOnClickListener{
             findNavController().navigate(R.id.action_mainMenuFragment_to_nav_request)
         }
+        binding.textViewRequestMainMenu.setOnClickListener {
+            findNavController().navigate(R.id.action_mainMenuFragment_to_nav_request)
+        }
 
         binding.imageButtonSurvey.setOnClickListener {
             findNavController().navigate(R.id.action_mainMenuFragment_to_nav_survey)
         }
+        binding.textViewSurveyMainMenu.setOnClickListener {
+            findNavController().navigate(R.id.action_mainMenuFragment_to_nav_survey)
+        }
 
+        binding.imageButtonInfo.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse("https://nutritionfacts.org/")
+            startActivity(intent)
+        }
+        binding.textViewMoreInfo.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse("https://nutritionfacts.org/")
+            startActivity(intent)
+        }
     }
 
         override fun onDestroyView() {
